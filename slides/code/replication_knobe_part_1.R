f78ffec6aff07e0e7f02f70425d31b10ca55802e
Max <53583040+alephmembeth@users.noreply.github.com>
2024-04-28 20:12:59 +0200

    Update slides

---
+++ slides/code/replication_knobe_part_1.R
@@ -0,0 +1,18 @@
+# read data
+df <- read.csv("replication_knobe_data_1.csv", sep = ';')
+print(df)
+
+# create fourfold table
+table <- xtabs (~ df$Condition + df$Answer)
+print(table)
+
+# calculate expected frequencies
+n <- sum(table)
+ef <- outer (rowSums(table), colSums(table)) / n
+print(ef)
+
+# calculate chi-squared test
+chisq.test(df$Condition, df$Answer)
+
+# calculate Fisher's exact test
+fisher.test(df$Condition, df$Answer)
